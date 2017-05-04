24c49addbc58acdcaaa9943233c22f5978c3f695
Austin Wilson <axwils2@gmail.com>
2017-05-03 23:46:45 -0500

    use local env variable instead of path location for json keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider: 'Google',
     google_project: 'austinandsabs',
     google_client_email: 'austin@austinandsabs.iam.gserviceaccount.com',
-    google_json_key_string: ENV["GOOGLE_JSON_KEY_STRING"]
+    google_json_key_string: ENV['GOOGLE_JSON_KEY_STRING']
   }
   config.storage = :fog
   config.fog_directory = 'austinandsabs-pics'
